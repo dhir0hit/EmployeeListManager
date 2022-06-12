e8ec9e8f5af0261429c2320a33d67f3355a21f8e
dhir0hit <rohit786dhir@gmail.com>
2022-06-11 20:13:36 -0400

    Created Controller and Views.

--- EmployeeListManager/App_Start/RouteConfig.cs
+++ EmployeeListManager/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace EmployeeListManager
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new {controller = "Home", action = "Index", id = UrlParameter.Optional}
+                defaults: new {controller = "EmployeeDB", action = "Index", id = UrlParameter.Optional}
             );
         }
     }
---
+++ EmployeeListManager/Controllers/EmployeeDBController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+
+namespace EmployeeListManager.Controllers
+{
+    public class EmployeeDBController : Controller
+    {
+        // GET
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
