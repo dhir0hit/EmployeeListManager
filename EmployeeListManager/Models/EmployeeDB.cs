018ee0e08e1e1581019629bcb6d83cad32afcdc3
17122001 <vanshikabhanot93@gmail.com>
2022-06-15 14:38:22 -0400

    added classes in model

--- EmployeeListManager/Models/EmployeeDB.cs
+++ EmployeeListManager/Models/EmployeeDB.cs
@@ -7,5 +7,37 @@ namespace EmployeeListManager.Models
 {
     public class EmployeeDB
     {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int EmployeeAge { get; set; }
+        public string DeptID { get; set; }
+        public string City { get; set; }
+        public int yearsExp { get; set; }
+        public Dept EmployeeDept { get; set; }
+        public int EmployeeCompanyID { get; set; }
+        public EmployeeCompany EmployeeCompanyName { get; set; }
+
+        public EmployeeDB()
+        {
+            EmployeeDept = new Dept();
+            EmployeeCompanyName = new EmployeeCompany();
+        }
+        public class Dept
+        {
+            public string Name { get; set; }
+            public int ID { get; set; }
+            public Dept() { }
+            public Dept(int id, string name)
+            {
+                ID = id;
+                Name = name;
+            }
+        }
+    }
+    public class EmployeeCompany
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
     }
 }
